c7dddb1d709f54d3f19cb43a22b55d7cb5f26405
kouta1237 <biz-kouta1237@outlook.jp>
2021-10-21 15:57:24 +0900

    Vuex作成～

--- src/components/Memo.vue
+++ src/components/Memo.vue
@@ -1,6 +1,14 @@
 <template> 
         <div class="wrapper text-center mb-16">
-         <h1>ここにメモ帳を表示</h1>
+         <h1 class="font-bold text-blue-500">好きなことを書いて頭の整理をする</h1>
+         <textarea name="" 
+                   id="" 
+                   class="border-2 
+                          w-10/12 
+                          h-96
+                          border-blue-500
+                          rounded">
+         </textarea>
          </div>
 </template>
 
