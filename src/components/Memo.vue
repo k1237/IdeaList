0501e88ebdb74793457a4264dc2580397fa88f24
kouta1237 <biz-kouta1237@outlook.jp>
2021-10-21 16:05:45 +0900

    Vuex~

--- src/components/Memo.vue
+++ src/components/Memo.vue
@@ -1,12 +1,12 @@
 <template> 
         <div class="wrapper text-center mb-16">
-         <h1 class="font-bold text-blue-500">好きなことを書いて頭の整理をする</h1>
+         <h1 class="font-bold ">好きなことを書いて頭の整理をする</h1>
          <textarea name="" 
                    id="" 
                    class="border-2 
                           w-10/12 
                           h-96
-                          border-blue-500
+                           border-black
                           rounded">
          </textarea>
          </div>
